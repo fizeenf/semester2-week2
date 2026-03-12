ce27f77048eec0a9f5de762b0935ce6679c8468d
fizeenf <lhxz0885@leeds.ac.uk>
2026-03-12 13:12:24 +0000

    week2

--- worksheet/task_2/cinema.py
+++ worksheet/task_2/cinema.py
@@ -11,39 +11,51 @@ import sqlite3
 
 
 def customer_tickets(conn, customer_id):
-    """
+    
     Return a list of tuples:
     (film_title, screen, price)
 
     Include only tickets purchased by the given customer_id.
     Order results by film title alphabetically.
-    """
-    curr.con.cursor()
-    res = cur.execute
+    
 
+    curr = con.cursor()
+    res = cur.execute("""
     SELECT films.title , screenings.screen , tickets.price
     FROM tickets 
-    JOIN 
+    JOIN screening ON tickets.screening_id = screenings.screening_id
+    JOIN films on screenings.file_id = films.file_id
+    WHERE ticket.customer_id = ? 
+    ORDER BY films.title ASC
+    """, (customer_id,))
 
-
-    
-    
+    return res.fetchall()
     pass
 
 
 def screening_sales(conn):
-    """
+    
     Return a list of tuples:
     (screening_id, film_title, tickets_sold)
 
     Include all screenings, even if tickets_sold is 0.
     Order results by tickets_sold descending.
-    """
+    
+    cur = conn.cursor()
+    res = cur.execute("""
+    SELECT screenings.screening_id, films.title, 
+    COUNT(tickets.ticket_id) AS tickets_sold
+    FROM screenings
+    JOIN films ON screenings.film_id = films.film_id 
+    LEFT JOIN tickets ON screenings.screening.id = tickets_screening_id
+    ORDER BY tickets_sold DESC
+    """)
+    return res.fetchall()
     pass
 
 
 def top_customers_by_spend(conn, limit):
-    """
+    
     Return a list of tuples:
     (customer_name, total_spent)
 
@@ -51,5 +63,17 @@ def top_customers_by_spend(conn, limit):
     Only include customers who have bought at least one ticket.
     Order by total_spent descending.
     Limit the number of rows returned to `limit`.
-    """
+
+    cur = conn.cursor()
+    res = cur.execute("""
+    SELECT customers.custome_name,
+     SUM (tickets.price) AS total_spent
+     FROM customer
+     JOIN tickets ON customers.customer_id = tickets.customer_id
+     GROUP BY customer.customer_id 
+     ORDER BY total_spent DESC
+     LIMIT ? 
+     """, (limit,))
+    
+    return res.fetchall()
     pass
